0e93a805d05ca6008df0e025d53c6218367b9ae8
ELIE-MANI <eliemaniraguha@gmail.com>
2026-03-17 18:18:36 +0200

    Add premium badge to package card

--- components/home/PackageCard.jsx
+++ components/home/PackageCard.jsx
@@ -3,9 +3,14 @@ import Image from 'next/image'
 export default function PackageCard({title,image,duration,people}) {
   return (
     <div className='bg-white rounded-lg overflow-hidden shadow'>
+     <div className='relative'>
       <Image src={image} alt={title} width={500} height={500}
       className='w-full h-56 object-cover'
       />
+      <div className='absolute top-3 left-3 bg-(--dark) tracking-wider font-medium text-white text-xs px-3 py-1'>
+      PREMIUM
+     </div>
+     </div>
      <div className='p-6'>
         <h3 className='text-2xl font-bold mt-2'>
             {title}
