45593fa867845447da36609194a8145a4c448a5d
ELIE-MANI <eliemaniraguha@gmail.com>
2026-03-17 19:29:20 +0200

    Add professional TourPackages section header

--- components/home/ToursPreview.jsx
+++ components/home/ToursPreview.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import PackageCard from './PackageCard'
+import Container from '../ui/Container'
 
 export default function ToursPreview() {
   return (
-    <div>
+    <div className='relative py-24'>
+         <div>
+        <p className='absolute top-6 left-1/2 transform -translate-x-1/2 bg-white uppercase text-sm text-center w-48 px-4 py-2 border font-semibold text-gray-700'>
+          Tour Packages
+        </p>
+        <h2 className='text-5xl font-bold text-center mt-6'>
+        Unforgettable Rwanda Tours
+        </h2>
+        <p className='mb-10 text-center text-gray-600 font-bold mt-8 max-w-2xl mx-auto'>
+          Carefully designed itineraries that showcase the best of Rwanda's wildlife, culture,
+           and landscapes
+        </p>
+        </div>
+        <Container>
+       <div className='mt-16'>     
       <PackageCard 
       title="Gorilla Encouter"
       image="/image/trekk.jpg"
@@ -17,6 +32,8 @@ export default function ToursPreview() {
        "Professional guide"
       ]}
       />
+      </div>
+      </Container>
     </div>
   )
 }
