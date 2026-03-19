db67a39aa36db917a4a512615d2e962aa0e0aa1c
ELIE-MANI <eliemaniraguha@gmail.com>
2026-03-19 10:46:42 +0200

    Add dynamic tour packages grid with map rendering

--- components/home/ToursPreview.jsx
+++ components/home/ToursPreview.jsx
@@ -2,6 +2,56 @@ import React from 'react'
 import PackageCard from './PackageCard'
 import Container from '../ui/Container'
 
+const packages = [
+  {
+    id:1,
+    title:"Gorilla Ecounter",
+    duration:"5 Days / 4 Nights",
+    people:"4-6 People",
+    price:"$2,500",
+    image:"/image/trekk.jpg",
+    highlights:[
+      "Gorilla trekking",
+      "Luxury Accomodation",
+      "Cultural Experience",
+      "Volcano  Hiking"
+    ]
+
+  },
+  {
+    id:2,
+    title:"Rwanda wildlife safaris",
+    duration:"4 Days / 3 Nights",
+    people:"4-6 People",
+    price:"$2,100",
+    image:"/image/akagera2.jpg",
+    highlights:[
+      "Big Five Game Driver",
+      "Boat Safari",
+      " Chimpanzee trek",
+      "Bisoke Hiking"
+    ]
+  },
+  {
+    id:3,
+    title:"Kigali & Nyungwe Adventure",
+    duration:"4 Days / 3 Nights",
+    people:"4-6 People",
+    price:"$4,500",
+    image:"/image/nyungwe.jpg",
+    highlights:[
+      "Kigali City Tour ",
+      "Canopy Walk & Zipline",
+      "Chimpanzee Trekking",
+      "Expert Guide"
+    ]
+  }
+]
+
+
+
+
+
 export default function ToursPreview() {
   return (
     <div className='relative py-24'>
@@ -18,20 +68,20 @@ export default function ToursPreview() {
         </p>
         </div>
         <Container>
-       <div className='mt-16'>     
-      <PackageCard 
-      title="Gorilla Encouter"
-      image="/image/trekk.jpg"
-      duration="5 days/ 4 nights"
-      people="2-6 people"
-       price="$2500"
-      highlights={[
-        "Gorilla trekking",
-       "Luxury accommodation",
-       "Cultural village visit",
-       "Professional guide"
-      ]}
-      />
+       <div className='mt-16 grid grid-cols-1 md:grid-cols-3 gap-8'>     
+       {packages.map((pkg) => (
+        <PackageCard 
+        
+        key={pkg.id}
+        title={pkg.title}
+        image={pkg.image}
+        duration={pkg.duration}
+        people={pkg.people}
+        price={pkg.price}
+        highlights={pkg.highlights} 
+        
+        />
+       ))}
       </div>
       </Container>
     </div>
