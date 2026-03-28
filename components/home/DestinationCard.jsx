f02f5517928d4ff1fd90cb671556b087e27c73d9
ELIE-MANI <eliemaniraguha@gmail.com>
2026-03-28 21:06:58 +0200

    Move destinations data to centralized file

--- components/home/DestinationCard.jsx
+++ components/home/DestinationCard.jsx
@@ -1,12 +1,12 @@
-import React from "react"
-import Image from "next/image"
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function DestinationCard({ title, image, description }) {
+export default function DestinationCard({ title, image, description,slug }) {
   return (
     <div className="group rounded-2xl overflow-hidden shadow-md hover:shadow-2xl transition duration-700">
-
       {/* IMAGE */}
-      <div className="relative w-full h-[420px]">
+      <div className="relative w-full h-105">
         <Image
           src={image}
           alt={title}
@@ -16,7 +16,7 @@ export default function DestinationCard({ title, image, description }) {
         />
 
         {/* OVERLAY */}
-        <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/20 to-transparent"></div>
+        <div className="absolute inset-0 bg-linear-to-t from-black/70 via-black/20 to-transparent"></div>
 
         {/* CONTENT */}
         <div className="absolute bottom-0 p-6 text-white">
@@ -24,19 +24,19 @@ export default function DestinationCard({ title, image, description }) {
             Destination
           </p>
 
-          <h3 className="text-2xl font-bold mt-1">
-            {title}
-          </h3>
+          <h3 className="text-2xl font-bold mt-1">{title}</h3>
 
           <p className="text-sm mt-2 text-gray-200 opacity-0 group-hover:opacity-100 transition duration-500">
             {description}
           </p>
-
+        
+        <Link href={`/destinations/${slug}`}>
           <button className="mt-4 text-sm border border-white px-4 py-2 uppercase tracking-wide hover:bg-white hover:text-black transition duration-300">
             Explore
           </button>
+          </Link>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- components/home/DestinationsPreview.jsx
+++ components/home/DestinationsPreview.jsx
@@ -1,49 +1,16 @@
-"use client"
+"use client";
 
-import React from "react"
-import DestinationCard from "./DestinationCard"
-import Container from "../ui/Container"
-import Link from "next/link"
+import React from "react";
+import DestinationCard from "./DestinationCard";
+import Container from "../ui/Container";
+import Link from "next/link";
+import { destinations } from "@/lib/data/destinations";
 
-const destinations = [
-  {
-    id: 1,
-    title: "Volcanoes National Park",
-    image: "/image/volcano.jpg",
-    description: "Home to endangered mountain gorillas",
-    featured: true
-  },
-  {
-    id: 2,
-    title: "Akagera National Park",
-    image: "/image/akagera.jpg",
-    description: "Big five safari experience"
-  },
-  {
-    id: 3,
-    title: "Nyungwe Forest National Park",
-    image: "/image/nyungwe.jpg",
-    description: "Ancient rainforest canopy walks"
-  },
-  {
-    id: 4,
-    title: "Lake Kivu",
-    image: "/image/lake.jpg",
-    description: "Relaxing lakeside retreats"
-  },
-  {
-    id: 5,
-    title: "Kibeho",
-    image: "/image/kibeho.jpg",
-    description: "Spiritual pilgrimage site"
-  }
-]
 
 export default function DestinationsPreview() {
   return (
     <section className="bg-[#E6D3A3] py-24">
       <Container>
-
         {/* HEADER */}
         <div className="text-center max-w-2xl mx-auto">
           <p className="inline-block bg-white uppercase text-xs px-4 py-2 border font-semibold text-gray-700 tracking-wider">
@@ -55,14 +22,13 @@ export default function DestinationsPreview() {
           </h2>
 
           <p className="text-gray-600 mt-6">
-            Discover Rwanda's most iconic locations, each offering unique wildlife encounters 
-            and unforgettable experiences.
+            Discover Rwanda's most iconic locations, each offering unique
+            wildlife encounters and unforgettable experiences.
           </p>
         </div>
 
         {/* FEATURED + GRID */}
         <div className="mt-16 grid md:grid-cols-2 gap-10">
-
           {/* FEATURED (BIG) */}
           <div className="md:col-span-2">
             <DestinationCard
@@ -79,9 +45,9 @@ export default function DestinationsPreview() {
               title={destination.title}
               image={destination.image}
               description={destination.description}
+              slug ={destination.slug}
             />
           ))}
-
         </div>
 
         {/* CTA */}
@@ -92,8 +58,7 @@ export default function DestinationsPreview() {
             </button>
           </Link>
         </div>
-
       </Container>
     </section>
-  )
+  );
 }
